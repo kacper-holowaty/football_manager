1bc1ce4952a84a172607c927e369265f8c79b31a
kacper-holowaty <kacper.holowaty@gmail.com>
2025-02-04 21:54:47 +0100

    :sparkles: Add filtering logic in club-list component

--- frontend/src/app/components/club/club-list/club-list.component.ts
+++ frontend/src/app/components/club/club-list/club-list.component.ts
@@ -4,18 +4,27 @@ import { Router } from '@angular/router';
 import { Club } from '../../../models/club.model';
 import { CountryService } from '../../../services/country.service';
 import { Location } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-club-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './club-list.component.html',
   styleUrl: './club-list.component.scss'
 })
 export class ClubListComponent implements OnInit {
   protected clubs?: Club[];
+  protected filteredClubs: Club[] = [];
   protected defaultBadgeUrl: string = "assets/empty_badge.png";
   protected countryCodes: Record<string, string> = {};
+  protected availableCountries: string[] = [];
+  
+  protected searchName: string = '';
+  protected selectedCountry: string = '';
+  protected onlyWithBadge: boolean = false;
 
   public constructor(private router: Router, private clubService: ClubService, private countryService: CountryService, private location: Location) {}
 
@@ -23,8 +32,11 @@ export class ClubListComponent implements OnInit {
     this.clubService.getAllClubs().subscribe({
       next: (clubs: Club[]) => {
         this.clubs = clubs;
+        this.filteredClubs = [...this.clubs];
+
         if (this.clubs.length > 0) {
           this.loadCountryFlags(this.clubs);
+          this.availableCountries = [...new Set(this.clubs.map((club) => club.address.country))];
         }
       },
       error: (error) => {
@@ -61,5 +73,13 @@ export class ClubListComponent implements OnInit {
 
   protected goHome(): void {
     this.router.navigate(['/']);
+  }
+
+  protected filterClubs(): void {
+    this.filteredClubs = this.clubs?.filter((club) =>
+      club.name.toLowerCase().includes(this.searchName.toLowerCase()) &&
+      (this.selectedCountry ? club.address.country === this.selectedCountry : true) &&
+      (!this.onlyWithBadge || club.badge !== null)
+    ) || [];
   }
 }
